9c586191bef2dc1c1a0a10deca6049624b6a8b54
Dylan Uys <dylan.uys@gmail.com>
2025-03-22 17:28:28 -0500

    moving env file creation to setup.sh

--- setup.sh
+++ setup.sh
@@ -19,3 +19,57 @@ $PREFIX npm install -g pm2@latest
 
 # install repository itself
 pip install -e . --use-pep517
+
+# Create miner.env if it doesn't exist
+if [ -f "miner.env" ]; then
+    echo "File 'miner.env' already exists. Skipping creation."
+else
+    cat > miner.env << 'EOL'
+# Subtensor Network Configuration:
+NETUID=                                        # Network UID options: 18, 301
+SUBTENSOR_NETWORK=                             # Networks: finney, test, local
+SUBTENSOR_CHAIN_ENDPOINT=
+                                               # Endpoints:
+                                               # - wss://entrypoint-finney.opentensor.ai:443
+                                               # - wss://test.finney.opentensor.ai:443/
+
+# Wallet Configuration:
+WALLET_NAME=default
+WALLET_HOTKEY=default
+
+# Miner Settings:
+AXON_PORT=
+BLACKLIST_FORCE_VALIDATOR_PERMIT=True          # Default setting to force validator permit for blacklisting
+EOL
+    echo "File 'miner.env' created."
+fi
+
+# Create validator.env if it doesn't exist
+if [ -f "validator.env" ]; then
+    echo "File 'validator.env' already exists. Skipping creation."
+else
+    cat > validator.env << 'EOL'
+NETUID=                                         # Netuids: 18 (for finney), 301 (for testnet)
+SUBTENSOR_NETWORK=                              # Networks: finney, test, local
+SUBTENSOR_CHAIN_ENDPOINT=
+                                                # Endpoints:
+                                                # - wss://entrypoint-finney.opentensor.ai:443
+                                                # - wss://test.finney.opentensor.ai:443/
+
+# Wallet Configuration:
+WALLET_NAME=default
+WALLET_HOTKEY=default
+
+# Validator Port Setting:
+AXON_PORT=
+PROXY_PORT=
+
+# API Keys:
+WANDB_API_KEY=      # https://wandb.ai/authorize
+CDS_API_KEY =       # https://github.com/Orpheus-AI/Zeus/blob/main/docs/Validating.md#ecmwf
+PROXY_API_KEY=      # Your Proxy API Key, you can generate it yourself
+EOL
+    echo "File 'validator.env' created."
+fi
+
+echo "Environment setup completed successfully."
